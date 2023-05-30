260d29170d433fccc0473b33b5ce8e2de46dfb1e
toni <easyrobotics2@gmail.com>
2023-05-30 13:34:48 +0200

    TODO nota

--- lista-compra-final/js/script.js
+++ lista-compra-final/js/script.js
@@ -1,6 +1,7 @@
 /**
  * TODO
- * 1- Implementar borrado completo
+ * 1- Implementar borrado completo 
+ * 
  *    de la lista al clicar en el boton Limpiar lista
  * 2- Al clicar en Añadir se añade un elemento al final de la lista
  *    en el texto del campo de texto
