483866b5b00f28992789a2e527562ed84d7fd8b9
toni <easyrobotics2@gmail.com>
2023-06-01 15:51:49 +0200

    Filter

--- lista-compra-final/js/script.js
+++ lista-compra-final/js/script.js
@@ -1,7 +1,17 @@
+//Datos precargados...
+const carrito = ["Manzanas", "Zumo de naranja", "Oreos", "Leche"];
+
 const itemForm = document.getElementById("item-form");
 const itemInput = document.getElementById("item-input");
 const itemList = document.getElementById("item-list");
+const itemFilter = document.getElementById("filter");
 
+//Carga de datos del carrito
+function loadCarrito(miCarrito) {
+  miCarrito.forEach((item) => {
+    createNewItem(item);
+  });
+}
 function addItem(evt) {
   evt.preventDefault();
   const newItem = itemInput.value;
@@ -12,7 +22,20 @@ function addItem(evt) {
   //insertar un nuevo elemento en la lista
   createNewItem(newItem);
 }
+function filterItems() {
+  const items = itemList.querySelectorAll("li");
+  const textToFilter = itemFilter.value;
 
+  items.forEach((item) => {
+    const txtItem = item.firstChild.textContent.toLocaleLowerCase();
+    if (txtItem.indexOf(textToFilter) != -1) {
+      item.style.display = "flex";
+    } else {
+      item.style.display = "none";
+    }
+    console.log(txtItem);
+  });
+}
 function createNewItem(item) {
   const li = document.createElement("li");
   li.className = "item";
@@ -20,7 +43,13 @@ function createNewItem(item) {
 
   const button = document.createElement("button");
   button.className = "remove-item btn-link text-red";
-
+  //Añadir evento de borrado
+  button.addEventListener("click", (evt) => {
+    //TODO confirmacion de borrado
+    const currentItem = evt.currentTarget.parentElement;
+    currentItem.remove();
+    console.log(evt.currentTarget.parentElement);
+  });
   const icon = document.createElement("i");
   icon.className = "fa-solid fa-xmark";
 
@@ -31,5 +60,9 @@ function createNewItem(item) {
   //Añadir el li entero al ul de la lista
   itemList.appendChild(li);
 }
-//Evento de envio del formulario
+
+//Event listeners
 itemForm.addEventListener("submit", addItem);
+itemFilter.addEventListener("input", filterItems);
+
+loadCarrito(carrito);
